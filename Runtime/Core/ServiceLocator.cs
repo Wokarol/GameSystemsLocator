b57dd33445d5ad0c144d5831f5c61d4793d3e284
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2026-02-19 20:21:25 +0100

    [Hotfix] Make it so the systems start hidden as to not pollute the scene view

--- Runtime/Core/ServiceLocator.cs
+++ Runtime/Core/ServiceLocator.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Wokarol.GameSystemsLocator.Core
 {
     public class ServiceLocator
@@ -43,6 +47,7 @@ namespace Wokarol.GameSystemsLocator.Core
             {
                 BindSystemsFromObject(root);
                 CreateSystemThatAreNotPresent(root);
+                HideTheObjectInEditorIfPossible(root);
             }
             else
             {
@@ -67,6 +72,13 @@ namespace Wokarol.GameSystemsLocator.Core
                     system.Value.BindInstance(systemHost.AddComponent(system.Key));
                 }
             }
+        }
+
+        private void HideTheObjectInEditorIfPossible(GameObject root)
+        {
+#if UNITY_EDITOR && UNITY_2019_1_OR_NEWER
+            SceneVisibilityManager.instance.Hide(root, true);
+#endif
         }
 
         /// <summary>
